fe505d05debbcd8d85429d9ff875c5ef9fded1cc
Palanath <pala@dusttoash.org>
2022-12-14 23:27:14 -0600

    Update LaunchSettingsWindow.java

--- src/pala/apps/arlith/app/guis/login/LaunchSettingsWindow.java
+++ src/pala/apps/arlith/app/guis/login/LaunchSettingsWindow.java
@@ -3,25 +3,25 @@ package pala.apps.arlith.app.guis.login;
 import java.io.IOException;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.stage.Stage;
 import pala.apps.arlith.api.Utilities;
 import pala.apps.arlith.app.server.Server;
+import pala.libs.generic.guis.ApplicationProperties;
+import pala.libs.generic.guis.Window;
 
-public class LaunchSettingsWindow {
+public class LaunchSettingsWindow extends Window {
 	private @FXML TextField localAddress, localPort, serverAddress, serverPort;
 	private @FXML Button launchServerButton;
-	private final LogInWindow source;
 
 	private @FXML void initialize() {
 		String port = String.valueOf(Utilities.DEFAULT_PORT);
 		localPort.setText(port);
 		serverAddress.setText(Utilities.DEFAULT_DESTINATION_ADDRESS);
 		serverPort.setText(port);
-	}
-
-	public LaunchSettingsWindow(LogInWindow source) {
-		this.source = source;
 	}
 
 	private @FXML void launchServer() {
@@ -47,5 +47,25 @@ public class LaunchSettingsWindow {
 		if (!serverAddress.getText().trim().isEmpty())
 			Utilities.setPreferredPort(Integer.parseInt(serverPort.getText().trim()));
 		System.out.println("Client endpoint set successfully.");
+	}
+
+	@Override
+	public void destroy() {
+
+	}
+
+	@Override
+	protected void show(Stage stage, ApplicationProperties properties) throws WindowLoadFailureException {
+		FXMLLoader loader = new FXMLLoader();
+		loader.setLocation(getClass().getResource("LaunchSettings.fxml"));
+		loader.setController(this);
+		try {
+			Scene scene = new Scene(loader.load());
+			scene.getStylesheets().add(properties.themeStylesheet.get());
+			stage.setScene(scene);
+		} catch (IOException e) {
+			throw new WindowLoadFailureException(e);
+		}
+
 	}
 }
