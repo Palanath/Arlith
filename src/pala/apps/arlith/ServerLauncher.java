07260025d7f5cc13159574def95869ca6020c5ba
Palanath <pala@dusttoash.org>
2022-12-17 18:56:40 -0600

    Update ServerLauncher.java

--- src/pala/apps/arlith/ServerLauncher.java
+++ src/pala/apps/arlith/ServerLauncher.java
@@ -13,6 +13,16 @@ public class ServerLauncher implements ApplicationLauncher {
 		System.out.println(
 				"Started the server on port: " + Utilities.getPreferredPort() + ". Close the program to terminate.");
 
+		// Problematic, non-standard Java implementation terminates when the main thread
+		// terminates. To solve this, we just put the main thread in a wait loop. This
+		// is nasty. I'll make a note to fix it later.
+		// Also, don't use Amazon AWS.
+		if (Arlith.getCLIParameters().checkFlag(false, "--main-thread-wait")) {
+			Object o = new Object();
+			synchronized (o) {
+				o.wait();
+			}
+		}
 	}
 
 }
