40c69ed8c3db8ca85a002198c60b7d03c1e0b21a
Palanath <pala@dusttoash.org>
2022-12-21 04:42:46 -0600

    Update ServerLauncher to not launch server threads as daemons

--- src/pala/apps/arlith/launchers/terminalserver/ServerLauncher.java
+++ src/pala/apps/arlith/launchers/terminalserver/ServerLauncher.java
@@ -1,6 +1,5 @@
 package pala.apps.arlith.launchers.terminalserver;
 
-import pala.apps.arlith.Arlith;
 import pala.apps.arlith.backend.server.ArlithServer;
 import pala.apps.arlith.launchers.ApplicationLauncher;
 import pala.apps.arlith.libraries.Utilities;
@@ -11,21 +10,10 @@ public class ServerLauncher implements ApplicationLauncher {
 	public void launchArlith(String... args) throws Exception {
 		ArlithServer server = new ArlithServer();
 		server.setPort(Utilities.getPreferredPort());
-		server.start();
 		server.setDaemon(false);
+		server.start();
 		System.out.println(
 				"Started the server on port: " + Utilities.getPreferredPort() + ". Close the program to terminate.");
-
-		// Problematic, non-standard Java implementation terminates when the main thread
-		// terminates. To solve this, we just put the main thread in a wait loop. This
-		// is nasty. I'll make a note to fix it later.
-		// Also, don't use Amazon AWS.
-		if (Arlith.getCLIParameters().checkFlag(false, "--main-thread-wait")) {
-			Object o = new Object();
-			synchronized (o) {
-				o.wait();
-			}
-		}
 	}
 
 }
