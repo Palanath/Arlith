8c58162c1d03ec8c48bd05122978f14200b6b61e
Palanath <pala@dusttoash.org>
2022-12-23 21:56:07 -0600

    Update server to use new logging API

--- src/pala/apps/arlith/backend/server/reqhandlers/LoginRequestHandler.java
+++ src/pala/apps/arlith/backend/server/reqhandlers/LoginRequestHandler.java
@@ -6,6 +6,7 @@ import pala.apps.arlith.backend.common.protocol.errors.RestrictedError;
 import pala.apps.arlith.backend.common.protocol.requests.LoginRequest;
 import pala.apps.arlith.backend.common.protocol.types.AuthTokenValue;
 import pala.apps.arlith.backend.common.protocol.types.LoginProblemValue;
+import pala.apps.arlith.backend.server.ArlithServer;
 import pala.apps.arlith.backend.server.contracts.serversystems.RequestConnection;
 import pala.apps.arlith.backend.server.contracts.world.ServerUser;
 import pala.apps.arlith.backend.server.systems.EventConnectionImpl;
@@ -39,14 +40,13 @@ public final class LoginRequestHandler extends SimpleRequestHandler<LoginRequest
 				client.sendError(new LoginError(LoginProblemValue.INVALID_PW));
 				return;
 			}
+			ArlithServer.getThreadLogger().std("The user " + user.getTag() + " just logged in.");
+			ArlithServer.changeThreadLoggerPurpose(user.getTag());
 			at = client.getServer().getAuthSystem().login(user);
 			client.getServer().getEventSystem()
 					.registerClient(new EventConnectionImpl(client.getConnection(), user.getGID()));
 			client.sendResult(new AuthTokenValue(at));
 			client.stopListening();
-//			BasicRequestResolver.log("The user: [" + client.getAccount().getUsername()
-//					+ "] logged in {EVENT_HANDLER}. IP: [" + client.getConnectionInfo().getInetAddress() + ':'
-//					+ client.getConnectionInfo().getPort() + "].");
 		}
 	}
 
