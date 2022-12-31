d2c9c687e59705182a436af401ebfc09e25d27ce
Palanath <pala@dusttoash.org>
2022-12-31 11:56:24 -0600

    Update LogInScene to show GUI

--- src/pala/apps/arlith/frontend/clientgui/LogInScene.java
+++ src/pala/apps/arlith/frontend/clientgui/LogInScene.java
@@ -1,11 +1,17 @@
 package pala.apps.arlith.frontend.clientgui;
 
+import java.io.IOException;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import pala.apps.arlith.backend.client.ArlithClientBuilder;
 import pala.apps.arlith.libraries.frontends.FrontendScene;
 
 public class LogInScene implements FrontendScene<ClientGUIFrontend> {
 	private final ClientGUIFrontend frontend;
 	private final ArlithClientBuilder builder;
+	private Scene scene;
 
 	public LogInScene(ClientGUIFrontend frontend, ArlithClientBuilder builder) {
 		this.frontend = frontend;
@@ -18,9 +24,18 @@ public class LogInScene implements FrontendScene<ClientGUIFrontend> {
 	}
 
 	@Override
-	public void show() {
-		// TODO Auto-generated method stub
-		
+	public void show() throws SceneShowFailureException {
+		FXMLLoader loader = new FXMLLoader(LogInScene.class.getResource("LogInGUI.fxml"));
+		loader.setController(this);
+		Parent root;
+		try {
+			root = loader.load();
+		} catch (IOException e) {
+			throw new SceneShowFailureException(e, this);
+		}
+		scene = new Scene(root);
+		frontend.getStage().setScene(scene);
+		frontend.getStage().show();
 	}
 
 }
