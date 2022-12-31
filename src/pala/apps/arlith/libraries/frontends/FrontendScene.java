3eb2833dcea760ab64628a117723a9b3e350973e
Palanath <pala@dusttoash.org>
2022-12-31 01:17:33 -0600

    Add documentation and new method to FrontendScene
    I decided to make it so that each FrontendScene has a show() method that
    causes that scene to be shown. Since each scene should have access to
    its parent Frontend (which is expected to store things like the Stage in
    a JavaFX-based Frontend), this should be easy to implement.
    
    This feels limiting but I cannot yet see how, so to avoid excessive
    pondering I've decided to just go with it.

--- src/pala/apps/arlith/libraries/frontends/FrontendScene.java
+++ src/pala/apps/arlith/libraries/frontends/FrontendScene.java
@@ -1,5 +1,16 @@
 package pala.apps.arlith.libraries.frontends;
 
 public interface FrontendScene<F extends Frontend> {
+	/**
+	 * Returns the {@link Frontend} main-class for the Frontend that this scene is a
+	 * part of.
+	 * 
+	 * @return The {@link Frontend} class that this scene belongs to.
+	 */
 	F getFrontend();
+
+	/**
+	 * Causes this scene to be shown to the user.
+	 */
+	void show();
 }
