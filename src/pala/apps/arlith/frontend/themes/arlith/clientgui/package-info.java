4cdfe552dbacaeaae4520e329a241c76ec7fbf6a
Palanath <pala@dusttoash.org>
2023-01-02 03:18:52 -0600

    Add package for clientgui in Arlith's default theme

---
+++ src/pala/apps/arlith/frontend/themes/arlith/clientgui/package-info.java
@@ -0,0 +1,9 @@
+/**
+ * Classes pertaining to presentation of the clientgui frontend through Arlith's
+ * default theme.
+ * 
+ * @author Palanath
+ * @see pala.apps.arlith.frontend.themes.arlith.ArlithTheme
+ *
+ */
+package pala.apps.arlith.frontend.themes.arlith.clientgui;
