6bb51e83f1a975913ce2a4023371161ea1586973
Palanath <pala@dusttoash.org>
2023-01-02 03:11:07 -0600

    Add interfacing package for frontends

---
+++ src/pala/apps/arlith/libraries/frontends/interfacing/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Provides library files for types implementing the abstract user interface
+ * model of separated function and input/output features from theme.
+ * 
+ * @author Palanath
+ *
+ */
+package pala.apps.arlith.libraries.frontends.interfacing;
