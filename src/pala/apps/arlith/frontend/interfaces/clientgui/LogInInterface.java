8b02d87aaa0d01b6de8cd1a7a0614ada6c87352e
Palanath <pala@dusttoash.org>
2023-01-02 03:13:46 -0600

    Add interface types for log-in UI

---
+++ src/pala/apps/arlith/frontend/interfaces/clientgui/LogInInterface.java
@@ -0,0 +1,11 @@
+package pala.apps.arlith.frontend.interfaces.clientgui;
+
+import pala.apps.arlith.libraries.frontends.interfacing.UserInterface;
+
+public interface LogInInterface extends UserInterface<LogInPresentation> {
+	/**
+	 * Triggers and performs the log in process. This method should be called
+	 * whenever the user attempts to log in.
+	 */
+	public void triggerLogIn();
+}
