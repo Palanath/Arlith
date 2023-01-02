5621282495863c08f68729ccf5e9e16e887eebe5
Palanath <pala@dusttoash.org>
2023-01-02 03:13:24 -0600

    Add package for interface specification of the client GUI

---
+++ src/pala/apps/arlith/frontend/interfaces/clientgui/package-info.java
@@ -0,0 +1,8 @@
+/**
+ * Stores the interface and presentation <code>interface</code>s for the Client
+ * GUI frontend.
+ * 
+ * @author Palanath
+ *
+ */
+package pala.apps.arlith.frontend.interfaces.clientgui;
