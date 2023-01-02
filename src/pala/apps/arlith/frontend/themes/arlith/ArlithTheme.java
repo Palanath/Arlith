78e65b50fe094be38f71933efcb5aeefc32a4204
Palanath <pala@dusttoash.org>
2023-01-02 03:17:13 -0600

    Add default Arlith theme class

---
+++ src/pala/apps/arlith/frontend/themes/arlith/ArlithTheme.java
@@ -0,0 +1,21 @@
+package pala.apps.arlith.frontend.themes.arlith;
+
+import pala.apps.arlith.libraries.frontends.interfacing.Theme;
+import pala.apps.arlith.libraries.frontends.interfacing.UserInterface;
+
+/**
+ * Arlith's default {@link Theme}. This class provides a presentation for every
+ * user interface that Arlith contains.
+ * 
+ * @author Palanath
+ *
+ */
+public class ArlithTheme implements Theme {
+
+	@Override
+	public <P> P supply(UserInterface<? super P> userInterface) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
