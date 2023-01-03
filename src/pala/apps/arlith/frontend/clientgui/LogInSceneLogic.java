c83f023e3584630368402f4ea205c16b9a2c25aa
Palanath <pala@dusttoash.org>
2023-01-03 01:45:23 -0600

    Extract LogInScene's logic to separate class

---
+++ src/pala/apps/arlith/frontend/clientgui/LogInSceneLogic.java
@@ -0,0 +1,13 @@
+package pala.apps.arlith.frontend.clientgui;
+
+import pala.apps.arlith.frontend.interfaces.clientgui.LogInInterface;
+
+public class LogInSceneLogic implements LogInInterface {
+
+	@Override
+	public void triggerLogIn() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
