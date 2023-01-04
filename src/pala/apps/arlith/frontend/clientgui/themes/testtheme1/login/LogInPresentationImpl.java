fe6caa35313dd0c3904fbf899d9447138e7238f0
Palanath <pala@dusttoash.org>
2023-01-04 01:13:06 -0600

    Add colored background to test theme log in UI

--- src/pala/apps/arlith/frontend/clientgui/themes/testtheme1/login/LogInPresentationImpl.java
+++ src/pala/apps/arlith/frontend/clientgui/themes/testtheme1/login/LogInPresentationImpl.java
@@ -1,6 +1,25 @@
 package pala.apps.arlith.frontend.clientgui.themes.testtheme1.login;
 
+import java.io.IOException;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.RadialGradient;
+import javafx.scene.paint.Stop;
+import javafx.scene.text.Text;
 import pala.apps.arlith.backend.common.protocol.types.LoginProblemValue;
 import pala.apps.arlith.frontend.clientgui.uispec.login.LogInLogic;
 import pala.apps.arlith.frontend.clientgui.uispec.login.LogInPresentation;
@@ -8,15 +27,45 @@ import pala.libs.generic.guis.Window.WindowLoadFailureException;
 
 public class LogInPresentationImpl implements LogInPresentation {
 	private final LogInLogic logic;
+	private Scene scene;
 
 	public LogInPresentationImpl(LogInLogic logic) {
 		this.logic = logic;
 	}
 
+	public @FXML BorderPane root;
+	public @FXML VBox logInBox, usernamePrompt, passwordPrompt;
+	public @FXML HBox usernamePromptTextSection, passwordPromptTextSection;
+	public @FXML Text usernamePromptText, usernamePromptTextRequiredAsterisk, passwordPromptText,
+			passwordPromptTextAsterisk, title;
+	public @FXML TextField usernamePromptField, passwordPromptField;
+	public @FXML Button logInButton;
+
+	private @FXML void initialize() {
+		root.setBackground(new Background(new BackgroundFill(Color.GRAY, CornerRadii.EMPTY, Insets.EMPTY),
+				new BackgroundFill(new RadialGradient(30, .2, .3, -.1, 1, true, CycleMethod.NO_CYCLE,
+						new Stop(0, Color.DARKGRAY), new Stop(.4, Color.TRANSPARENT)), CornerRadii.EMPTY, Insets.EMPTY),
+				new BackgroundFill(new RadialGradient(0, 0, 1, .7, 1, true, CycleMethod.NO_CYCLE,
+						new Stop(0, Color.DARKGRAY), new Stop(.4, Color.TRANSPARENT)), CornerRadii.EMPTY, Insets.EMPTY),
+				new BackgroundFill(
+						new RadialGradient(-30, -.1, .2, .9, 1, true, CycleMethod.NO_CYCLE,
+								new Stop(0, Color.color(.58, .58, .58)), new Stop(.4, Color.TRANSPARENT)),
+						CornerRadii.EMPTY, Insets.EMPTY)));
+	}
+
 	@Override
 	public Scene getScene() throws WindowLoadFailureException {
-		// TODO Auto-generated method stub
-		return null;
+		if (scene != null)
+			return scene;
+		FXMLLoader loader = new FXMLLoader(LogInPresentationImpl.class.getResource("LogInGUI.fxml"));
+		loader.setController(this);
+		Parent parent;
+		try {
+			parent = loader.load();
+		} catch (IOException e) {
+			throw new WindowLoadFailureException(e);
+		}
+		return scene = new Scene(parent);
 	}
 
 	@Override
