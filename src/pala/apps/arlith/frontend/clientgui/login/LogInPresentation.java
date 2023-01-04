8ef446712cc94b21cf9f20b2a300a0b259172bab
Palanath <pala@dusttoash.org>
2023-01-03 20:56:05 -0600

    Add methods to lock/unlock log in GUI when connecting

--- src/pala/apps/arlith/frontend/clientgui/login/LogInPresentation.java
+++ src/pala/apps/arlith/frontend/clientgui/login/LogInPresentation.java
@@ -24,9 +24,28 @@ public interface LogInPresentation {
 	 * Indicates to the user that the specified type of problem arose while
 	 * attempting to log the user in.
 	 * 
-	 * @param problem
+	 * @param problem The type of problem that occurred.
 	 */
 	void showLoginProblem(LoginProblemValue problem);
+
+	/**
+	 * Prevents the user from triggering a <code>log-in</code> (via
+	 * {@link LogInInterface#triggerLogIn()}) until {@link #unlockUIForLoggingIn()}
+	 * is called. The UI should remain responsive so as to indicate to the user that
+	 * the application is still alive, and should, preferably, also indicate to the
+	 * user that the application is currently attempting to log in. If this method
+	 * is called more than once before calling {@link #unlockUIForLoggingIn()}, the
+	 * subsequent calls have no effect.
+	 */
+	void lockUIForLoggingIn();
+
+	/**
+	 * Releases the restriction that bars the user from triggering a
+	 * <code>log-in</code> (essentially, undoes the effects of
+	 * {@link #lockUIForLoggingIn()}). This method is called by the Log In scene's
+	 * logic class while the client attempts to log in to the server, so that 
+	 */
+	void unlockUIForLoggingIn();
 
 	/**
 	 * Enables this presentation to start receiving user input so that it may
