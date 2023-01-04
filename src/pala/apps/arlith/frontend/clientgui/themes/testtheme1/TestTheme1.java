9d6962bb0734fa46d81e1ff2b552f33a78b644b4
Palanath <pala@dusttoash.org>
2023-01-03 23:04:22 -0600

    Add classes and resources for a test theme

---
+++ src/pala/apps/arlith/frontend/clientgui/themes/testtheme1/TestTheme1.java
@@ -0,0 +1,15 @@
+package pala.apps.arlith.frontend.clientgui.themes.testtheme1;
+
+import pala.apps.arlith.frontend.clientgui.Logic;
+import pala.apps.arlith.frontend.clientgui.Presentation;
+import pala.apps.arlith.frontend.clientgui.Theme;
+
+public class TestTheme1 implements Theme {
+
+	@Override
+	public <P extends Presentation<L>, L extends Logic<P>> P supply(L userInterface) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/pala/apps/arlith/frontend/clientgui/themes/testtheme1/login/LogInPresentationImpl.java
@@ -0,0 +1,46 @@
+package pala.apps.arlith.frontend.clientgui.themes.testtheme1.login;
+
+import javafx.scene.Scene;
+import pala.apps.arlith.backend.common.protocol.types.LoginProblemValue;
+import pala.apps.arlith.frontend.clientgui.uispec.login.LogInPresentation;
+import pala.libs.generic.guis.Window.WindowLoadFailureException;
+
+public class LogInPresentationImpl implements LogInPresentation {
+
+	@Override
+	public Scene getScene() throws WindowLoadFailureException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String getPassword() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void showLoginProblem(LoginProblemValue problem) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void lockUIForLoggingIn() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void unlockUIForLoggingIn() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
