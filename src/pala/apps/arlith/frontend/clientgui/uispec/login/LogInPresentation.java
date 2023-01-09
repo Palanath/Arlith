281b19822093b415ad6a0e9e8b9c9899e918904d
Palanath <pala@dusttoash.org>
2023-01-08 23:09:03 -0600

    Update LogInPresentation documentation

--- src/pala/apps/arlith/frontend/clientgui/uispec/login/LogInPresentation.java
+++ src/pala/apps/arlith/frontend/clientgui/uispec/login/LogInPresentation.java
@@ -3,6 +3,43 @@ package pala.apps.arlith.frontend.clientgui.uispec.login;
 import pala.apps.arlith.backend.common.protocol.types.LoginProblemValue;
 import pala.apps.arlith.frontend.clientgui.Presentation;
 
+/**
+ * <p>
+ * Outline class for the presentation of the initial scene shown to the user.
+ * This scene allows the user to log in or create an account. This scene is
+ * responsible for obtaining the information needed to <i>authenticate</i> the
+ * user.
+ * </p>
+ * <p>
+ * Authentication in can be done either through (1) a uniquely identifying
+ * <i>log-in identifier</i> and password or through (2) a username, an email
+ * address, a password, and optionally, a phone number. In case (1), the
+ * information is used to log the user in to an already existing account, and in
+ * case (2), the information is used to create the user a new account.
+ * </p>
+ * <p>
+ * Typically, this information is presented to the user by two, distinct menus:
+ * </p>
+ * <ul>
+ * <li>One for <em>Logging In</em> which contains:
+ * <ul>
+ * <li>A prompt for the user's username/email/phone-number</li>
+ * <li>A password prompt</li>
+ * </ul>
+ * </li>
+ * <li>One for <em>Account Creation</em> which contains:
+ * <ul>
+ * <li>A username prompt</li>
+ * <li>An email prompt</li>
+ * <li>A phone number prompt (which can be left blank)</li>
+ * <li>and a password prompt</li>
+ * </ul>
+ * </li>
+ * </ul>
+ * 
+ * @author Palanath
+ *
+ */
 public interface LogInPresentation extends Presentation<LogInLogic> {
 	/**
 	 * Retrieves the log in identifier that the user has entered to attempt to log
