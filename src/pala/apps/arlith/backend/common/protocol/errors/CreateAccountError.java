ca0e1257ab64ebf0f09d4e5b2a89a4beeda52b33
Palanath <pala@dusttoash.org>
2023-01-13 19:59:37 -0600

    Update CreateAccountError constructors

--- src/pala/apps/arlith/backend/common/protocol/errors/CreateAccountError.java
+++ src/pala/apps/arlith/backend/common/protocol/errors/CreateAccountError.java
@@ -40,6 +40,14 @@ public class CreateAccountError extends CommunicationProtocolError {
 		this.type = type;
 	}
 
+	public CreateAccountError(CreateAccountProblem type, String message) {
+		this(new EnumValue<>(type), message);
+	}
+
+	public CreateAccountError(CreateAccountProblem type) {
+		this(new EnumValue<>(type));
+	}
+
 	public static final String ERROR_TYPE = "create-account";
 	private static final String TYPE_KEY = "type";
 
