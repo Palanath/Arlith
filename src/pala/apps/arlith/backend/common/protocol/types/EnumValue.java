f3c3bc7dc6bb6e84561c1f0271eb0dd7f4324a6f
Palanath <pala@dusttoash.org>
2023-01-13 19:55:53 -0600

    Update EnumValue

--- src/pala/apps/arlith/backend/common/protocol/types/EnumValue.java
+++ src/pala/apps/arlith/backend/common/protocol/types/EnumValue.java
@@ -1,6 +1,8 @@
 package pala.apps.arlith.backend.common.protocol.types;
 
+import pala.apps.arlith.backend.common.protocol.errors.CreateAccountError.CreateAccountProblemValue;
 import pala.apps.arlith.backend.common.protocol.meta.CommunicationProtocolConstructionError;
+import pala.libs.generic.json.JSONConstant;
 import pala.libs.generic.json.JSONNumber;
 import pala.libs.generic.json.JSONValue;
 
@@ -23,6 +25,22 @@ public class EnumValue<E extends Enum<E>> implements CommunicationProtocolType {
 	@Override
 	public JSONValue json() {
 		return new JSONNumber(e.ordinal());
+	}
+
+	/**
+	 * Returns an {@link EnumValue} representing the provided argument if the
+	 * provided argument is not {@link JSONConstant#NULL}, otherwise, returns
+	 * <code>null</code>. This is essentially the "<code>null</code>-safe"
+	 * constructor/factory function for {@link EnumValue}s.
+	 * 
+	 * @param value The {@link JSONValue} to get the {@link EnumValue} from, which
+	 *              may represent <code>null</code> (by being
+	 *              {@link JSONConstant#NULL}).
+	 * @return <code>null</code> or an {@link EnumValue}, whichever represents the
+	 *         provided argument.
+	 */
+	public static <E extends Enum<E>> EnumValue<E> fromNullable(JSONValue value, Class<E> enumType) {
+		return value == JSONConstant.NULL ? null : new EnumValue<>(value, enumType);
 	}
 
 }
