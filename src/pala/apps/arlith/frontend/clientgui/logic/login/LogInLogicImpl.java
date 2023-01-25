b52b12f0e35357b35e90ace511bcd752450ba8eb
Palanath <pala@dusttoash.org>
2023-01-25 02:07:41 -0600

    Implement phone # trigger check in LogInLogicImpl

--- src/pala/apps/arlith/frontend/clientgui/logic/login/LogInLogicImpl.java
+++ src/pala/apps/arlith/frontend/clientgui/logic/login/LogInLogicImpl.java
@@ -233,8 +233,30 @@ public class LogInLogicImpl implements LogInLogic {
 
 	@Override
 	public void triggerCheckPhoneNumber() {
-		// TODO Auto-generated method stub
-
+		LogInPresentationWithLiveInputResponse presentation = (LogInPresentationWithLiveInputResponse) this.presentation;
+		String phone = presentation.getPhoneNumber();
+		if (phone.isEmpty()) {
+			presentation.showPhoneNumberError(null);
+			return;
+		}
+		if (phone.startsWith("+"))
+			phone = phone.substring(1);
+		if (phone.isEmpty())
+			presentation.showPhoneNumberError(new Issue(LogInPresentationWithLiveInputResponse.Severity.ERROR,
+					"Enter phone # (with country code) after '+'", -1));
+		// Australian phone numbers can be as many as 15 digits long, apparently.
+		// Once phone numbers are actually used, this will need to be more precisely
+		// sanitized.
+		else if (phone.length() > 15)
+			presentation.showPhoneNumberError(
+					new Issue(LogInPresentationWithLiveInputResponse.Severity.ERROR, "Phone # too long.", -1));
+		else
+			for (int i = 0; i < phone.length(); i++)
+				if (!Character.isDigit(phone.charAt(i))) {
+					presentation.showPhoneNumberError(new Issue(LogInPresentationWithLiveInputResponse.Severity.ERROR,
+							"'" + phone.charAt(i) + "' is not a digit.", i));
+					return;
+				}
 	}
 
 }
