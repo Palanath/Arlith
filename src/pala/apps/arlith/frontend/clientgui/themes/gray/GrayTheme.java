b00babfa9e3bd7490d0f92989cceb3d15e7337ed
Palanath <pala@dusttoash.org>
2023-02-03 02:52:01 -0600

    Update GrayTheme to map HomeLogic appropriately

--- src/pala/apps/arlith/frontend/clientgui/themes/gray/GrayTheme.java
+++ src/pala/apps/arlith/frontend/clientgui/themes/gray/GrayTheme.java
@@ -3,7 +3,9 @@ package pala.apps.arlith.frontend.clientgui.themes.gray;
 import pala.apps.arlith.frontend.clientgui.Logic;
 import pala.apps.arlith.frontend.clientgui.Presentation;
 import pala.apps.arlith.frontend.clientgui.Theme;
+import pala.apps.arlith.frontend.clientgui.themes.gray.home.HomePresentationImpl;
 import pala.apps.arlith.frontend.clientgui.themes.gray.login.LogInPresentationImpl;
+import pala.apps.arlith.frontend.clientgui.uispec.home.HomeLogic;
 import pala.apps.arlith.frontend.clientgui.uispec.login.LogInLogic;
 
 public class GrayTheme implements Theme {
@@ -13,6 +15,8 @@ public class GrayTheme implements Theme {
 	public <P extends Presentation<L>, L extends Logic<P>> P supply(L userInterface) {
 		if (userInterface instanceof LogInLogic)
 			return (P) new LogInPresentationImpl((LogInLogic) userInterface);
+		else if (userInterface instanceof HomeLogic)
+			return (P) new HomePresentationImpl();
 		return null;
 	}
 
