047e54d827fbc2b70c556fd78bb2bdbbfaba76cb
Palanath <pala@dusttoash.org>
2023-02-03 02:51:50 -0600

    Create HomeLogicImpl

---
+++ src/pala/apps/arlith/frontend/clientgui/logic/home/HomeLogicImpl.java
@@ -0,0 +1,14 @@
+package pala.apps.arlith.frontend.clientgui.logic.home;
+
+import pala.apps.arlith.frontend.clientgui.uispec.home.HomeLogic;
+import pala.apps.arlith.frontend.clientgui.uispec.home.HomePresentation;
+
+public class HomeLogicImpl implements HomeLogic {
+
+	@Override
+	public void hook(HomePresentation presentation) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
