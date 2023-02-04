d1e0c6389def3cec1a392d1474f18c685ac7f209
Palanath <pala@dusttoash.org>
2023-02-04 09:27:08 -0600

    Update HomePresentationImpl to access fx IDed nodes

--- src/pala/apps/arlith/frontend/clientgui/themes/gray/home/HomePresentationImpl.java
+++ src/pala/apps/arlith/frontend/clientgui/themes/gray/home/HomePresentationImpl.java
@@ -5,11 +5,27 @@ import java.io.IOException;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 import pala.apps.arlith.frontend.clientgui.uispec.home.HomePresentation;
 import pala.libs.generic.guis.Window.WindowLoadFailureException;
 
 public class HomePresentationImpl implements HomePresentation {
+
+	/**
+	 * List containing rectangular {@link UserListItem}s on the left-hand side of
+	 * the UI.
+	 */
+	private @FXML VBox userList;
+	/**
+	 * {@link VBox} where messages are shown. This {@link VBox} physically appears
+	 * on the right hand side of the home UI. It contains {@link HBox}es whenver
+	 * there are messages being shown in it. Each {@link HBox} represents a
+	 * <i>block</i> of messages from a single user. A block of messages will have
+	 * one or more messages in it.
+	 */
+	private @FXML VBox messagePane;
 
 	private @FXML void initialize() {
 
