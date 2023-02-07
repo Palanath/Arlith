9b72aaff0145b573b3505df6cddf23370cfbf49f
Palanath <pala@dusttoash.org>
2023-02-06 18:33:40 -0600

    Update documentation for UnknownCommStateException

--- src/pala/apps/arlith/libraries/networking/UnknownCommStateException.java
+++ src/pala/apps/arlith/libraries/networking/UnknownCommStateException.java
@@ -3,11 +3,11 @@ package pala.apps.arlith.libraries.networking;
 import java.io.IOException;
 
 /**
- * Used to signify that the state of a {@link Communicator}, regarding
- * connection between its endpoint, is unknown. This can be the result of a
- * malformed block, or if operations on the {@link Communicator}'s underlying
- * stream throw an {@link IOException}. Any time the class cannot guarantee that
- * a whole block was fully read will also result in an
+ * Used to signify that the state of a {@link Connection}, regarding connection
+ * between its endpoint, is unknown. This can be the result of a malformed
+ * block, or if operations on the {@link Connection}'s underlying stream throw
+ * an {@link IOException}. Any time the class cannot guarantee that a whole
+ * block was fully read will also result in an
  * {@link UnknownCommStateException}.
  * 
  * @author Palanath
