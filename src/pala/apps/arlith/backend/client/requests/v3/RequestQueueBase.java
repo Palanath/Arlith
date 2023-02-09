41edee3112ecad210f39566a79c55e846e0f68d1
Palanath <pala@dusttoash.org>
2023-02-08 18:57:03 -0600

    Update RequestQueueBase

--- src/pala/apps/arlith/backend/client/requests/v3/RequestQueueBase.java
+++ src/pala/apps/arlith/backend/client/requests/v3/RequestQueueBase.java
@@ -1,34 +1,144 @@
 package pala.apps.arlith.backend.client.requests.v3;
 
+import java.util.concurrent.LinkedBlockingQueue;
 import java.util.function.Consumer;
 
+import pala.apps.arlith.backend.client.ClientNetworkingBase;
 import pala.apps.arlith.backend.client.requests.Inquiry;
 import pala.apps.arlith.backend.common.protocol.errors.CommunicationProtocolError;
+import pala.apps.arlith.libraries.networking.Connection;
 
+/**
+ * <p>
+ * A standard implementation of {@link RequestQueue}. This class keeps track of
+ * a {@link #requestQueue request queue} and a {@link #queueThread queue thread}
+ * which it uses to manage queing. Inquiries can be made on the calling thread
+ * (as specified by {@link RequestSerializer} using {@link #inquire(Inquiry)},
+ * or be made on the {@link #queueThread} (as specified by {@link RequestQueue})
+ * {@link #queue(Inquiry, Consumer)} or
+ * {@link #queue(Inquiry, Consumer, Consumer)}.
+ * </p>
+ * <p>
+ * This class only instantiates a new {@link #queueThread queue thread} whenever
+ * new requests are queued. If the queue thread is able to complete all the
+ * requests in the queue, it blocks until a new item is put into the queue.
+ * </p>
+ * <p>
+ * This class maintains a private {@link #connectionLock lock} that is used to
+ * synchronize utilization of the {@link #getConnection() connection} provided
+ * by {@link RequestSerializerBase}. The lock is synchronized over by the
+ * {@link #queueThread queue thread} whenever it uses the
+ * {@link #getConnection() connection} and by the {@link #inquire(Inquiry)}
+ * method.
+ * </p>
+ * <p>
+ * The {@link ClientNetworkingBase} class utilizes <code>this</code> object for
+ * synchronizing {@link #start()} and {@link #stop()}. This class also
+ * synchronizes over <code>this</code> when assuring that the queue thread is
+ * prepared (and preparing it, if it isn't) so that the queued requests can be
+ * made.
+ * </p>
+ * 
+ * @author Palanath
+ *
+ */
 public abstract class RequestQueueBase extends RequestSerializerBase implements RequestQueue {
+
+	/**
+	 * Object used for synchronizing use of the {@link #getConnection() Connection}
+	 * being used. This object is synchronized over in the {@link #queueThread} (see
+	 * {@link #assureQueueThreadReady()}) and in {@link #inquire(Inquiry)}, both of
+	 * which run {@link Inquiry Inquiries} using the {@link Connection}.
+	 */
+	private final Object connectionLock = new Object();
+
+	private volatile Thread queueThread;
+	private final LinkedBlockingQueue<Request<?>> requestQueue = new LinkedBlockingQueue<>();
+
+	private class Request<R> {
+		private final Inquiry<? extends R> inquiry;
+		private final Consumer<? super R> resultHandler;
+		private final Consumer<? super Throwable> errorHandler;
+
+		public Request(Inquiry<? extends R> inquiry, Consumer<? super R> resultHandler,
+				Consumer<? super Throwable> errorHandler) {
+			this.inquiry = inquiry;
+			this.resultHandler = resultHandler;
+			this.errorHandler = errorHandler;
+		}
+
+		/**
+		 * <p>
+		 * Performs this {@link Request}, passing the result to the
+		 * {@link #resultHandler}, or the error to the {@link #errorHandler}, depending
+		 * on the way in which the inquiry completes.
+		 * </p>
+		 * <p>
+		 * Errors occurring from the call to the handler that gets executed are ignored.
+		 * </p>
+		 * 
+		 * @throws Throwable
+		 */
+		private void perform() {
+			R result;
+			try {
+				synchronized (connectionLock) {
+					result = inquiry.inquire(getConnection());
+				}
+			} catch (Throwable e) {
+				try {
+					errorHandler.accept(e);
+				} catch (Throwable e2) {
+				}
+				return;
+			}
+			try {
+				resultHandler.accept(result);
+			} catch (Throwable e) {
+			}
+		}
+	}
+
+	@Override
+	public <R> R inquire(Inquiry<? extends R> inquiry) throws CommunicationProtocolError {
+		synchronized (connectionLock) {
+			return super.inquire(inquiry);
+		}
+	}
+
 	@Override
 	public <R> void queue(Inquiry<? extends R> inquiry, Consumer<? super R> resultHandler,
 			Consumer<? super Throwable> errorHandler) {
-		// TODO Auto-generated method stub
-
+		try {
+			requestQueue.put(new Request<R>(inquiry, resultHandler, errorHandler));
+		} catch (InterruptedException e) {
+		}
+		assureQueueThreadReady();
 	}
 
-	@Override
-	public synchronized <R> R inquire(Inquiry<? extends R> inquiry) throws CommunicationProtocolError {
-		// TODO Auto-generated method stub
-		return super.inquire(inquiry);
-	}
+	private synchronized void assureQueueThreadReady() {
+		if (!isRunning())
+			throw new IllegalStateException("Cannot run queued inquiries while the RequestQueueBase is stopped.");
+		if (queueThread == null) {
+			queueThread = new Thread(() -> {
+				while (isRunning())
+					try {
+						requestQueue.take().perform();
+					} catch (InterruptedException e) {
+						if (!isRunning())
+							return;
+					}
+			});
+			queueThread.start();
+		}
 
-	@Override
-	public void start() {
-		// TODO Auto-generated method stub
-		super.start();
 	}
 
 	@Override
 	public void stop() {
-		// TODO Auto-generated method stub
 		super.stop();
+		if (queueThread != null)
+			queueThread.interrupt();
 	}
 
 }
