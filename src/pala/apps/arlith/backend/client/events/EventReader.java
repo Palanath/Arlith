6dd031b4fa9cba779ebe619f5c0db85ac1ce05f4
Palanath <pala@dusttoash.org>
2023-02-08 18:59:25 -0600

    Update EventReader

--- src/pala/apps/arlith/backend/client/events/EventReader.java
+++ src/pala/apps/arlith/backend/client/events/EventReader.java
@@ -2,9 +2,12 @@ package pala.apps.arlith.backend.client.events;
 
 import pala.apps.arlith.backend.common.protocol.IllegalCommunicationProtocolException;
 import pala.apps.arlith.backend.common.protocol.errors.CommunicationProtocolError;
+import pala.apps.arlith.libraries.networking.BlockException;
+import pala.apps.arlith.libraries.networking.Connection;
+import pala.apps.arlith.libraries.networking.UnknownCommStateException;
 import pala.apps.arlith.libraries.networking.scp.CommunicationConnection;
 
 public interface EventReader {
-	public EventInstance<?> apply(CommunicationConnection c)
-			throws IllegalCommunicationProtocolException, ClassCastException, IllegalArgumentException, CommunicationProtocolError;
+	public EventInstance<?> apply(Connection c) throws IllegalCommunicationProtocolException, ClassCastException,
+			IllegalArgumentException, CommunicationProtocolError, UnknownCommStateException, BlockException;
 }
