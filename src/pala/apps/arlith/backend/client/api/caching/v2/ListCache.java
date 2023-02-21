9e6b979d9c165423a1ce4f710119df79b5c7532a
Palanath <pala@dusttoash.org>
2023-02-21 16:59:59 -0600

    Update ListCache

--- src/pala/apps/arlith/backend/client/api/caching/v2/ListCache.java
+++ src/pala/apps/arlith/backend/client/api/caching/v2/ListCache.java
@@ -1,12 +1,15 @@
 package pala.apps.arlith.backend.client.api.caching.v2;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Supplier;
 
 import pala.apps.arlith.backend.client.requests.Inquiry;
 import pala.apps.arlith.backend.client.requests.v3.RequestQueue;
+import pala.apps.arlith.backend.common.protocol.errors.CommunicationProtocolError;
 import pala.apps.arlith.backend.common.protocol.types.ListValue;
 
 public class ListCache<O> extends NewCache<List<O>> {
@@ -42,6 +45,63 @@ public class ListCache<O> extends NewCache<List<O>> {
 	public ListCache(Supplier<? extends Inquiry<? extends ListValue<? extends O>>> inquirySupplier,
 			RequestQueue requestQueue) {
 		this(inquirySupplier, a -> a, requestQueue);
+	}
+
+	/**
+	 * Same as {@link #get()}, but the result is unmodifiable. This method calls
+	 * {@link #get()} and, if the result is not <code>null</code>, calls
+	 * {@link Collections#unmodifiableList(List)} and returns the result of that. If
+	 * the result of {@link #get()} is <code>null</code>, this method returns
+	 * <code>null</code>.
+	 * 
+	 * @return The list, but unmodifiable.
+	 * @throws CommunicationProtocolError Iff {@link #get()} throws a
+	 *                                    {@link CommunicationProtocolError}.
+	 */
+	public List<O> getUnmodifiable() throws CommunicationProtocolError {
+		List<O> value = get();
+		return value == null ? null : Collections.unmodifiableList(value);
+	}
+
+	/**
+	 * <p>
+	 * Same as {@link #get(Consumer, Consumer)}, but the {@link List} provided to
+	 * the <code>resultHandler</code> is unmodifiable. If querying the cache results
+	 * in the value <code>null</code> then <code>null</code> is provided to the
+	 * <code>resultHandler</code>. Otherwise, the list retrieved from the cache is
+	 * passed to {@link Collections#unmodifiableList(List)}, and the result of that
+	 * is given to <code>resultHandler</code>.
+	 * </p>
+	 * <p>
+	 * Note that the cache query may cause the cache to try to populate itself,
+	 * which involves sending a request.
+	 * </p>
+	 * 
+	 * @param resultHandler The {@link Consumer} to handle the result of the cache
+	 *                      query (either <code>null</code> or an unmodifiable
+	 *                      {@link List}). The {@link Consumer} can be
+	 *                      <code>null</code>, in which case, the result is
+	 *                      discarded.
+	 * @param errorHandler  The {@link Consumer} to handle the error. The error
+	 *                      handler can be <code>null</code>, in which case any
+	 *                      errors are simply discarded.
+	 */
+	public void getUnmodifiable(Consumer<? super List<O>> resultHandler, Consumer<? super Throwable> errorHandler) {
+		super.get(a -> resultHandler.accept(a == null ? null : Collections.unmodifiableList(a)), errorHandler);
+	}
+
+	/**
+	 * Calls {@link #getUnmodifiable(Consumer, Consumer)}, but passes
+	 * <code>null</code> for the <code>errorHandler</code>.
+	 * 
+	 * @param resultHandler The {@link Consumer} to handle the result of the cache
+	 *                      query (the result being either <code>null</code> or an
+	 *                      unmodifiable {@link List}). The {@link Consumer} can be
+	 *                      <code>null</code>, in which case the result is
+	 *                      discarded.
+	 */
+	public void getUnmodifiable(Consumer<? super List<O>> resultHandler) {
+		super.get(a -> resultHandler.accept(a == null ? null : Collections.unmodifiableList(a)));
 	}
 
 }
