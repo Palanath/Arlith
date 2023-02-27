29822c3b14a2d475a9cf1c4721a4b15e63f8bd1e
Palanath <pala@dusttoash.org>
2023-02-27 03:41:46 -0600

    Document and update Mask

--- src/pala/apps/arlith/libraries/watchables/Mask.java
+++ src/pala/apps/arlith/libraries/watchables/Mask.java
@@ -2,6 +2,22 @@ package pala.apps.arlith.libraries.watchables;
 
 import java.util.function.Function;
 
+/**
+ * A concrete implementation of {@link DerivativeWatchable} that takes in a
+ * specified {@link Watchable} and a {@link Function} to convert the value of
+ * that {@link Watchable} to the value of this {@link Mask}. This {@link Mask}
+ * can be watched for changes, just as its source {@link Watchable} can be, but
+ * the value provided to {@link Watcher}s of this {@link Mask} is transformed,
+ * from the source {@link Watchable}'s value, by the {@link Function} specified
+ * upon construction of this {@link Mask}. A {@link Mask}
+ * 
+ * @author Palanath
+ *
+ * @param <F> The type of the source {@link Watchable}.
+ * @param <T> The type of this {@link Mask} as a {@link Watchable}.
+ *            {@link Watcher} that see this {@link Mask}'s changes expect this
+ *            type of value.
+ */
 public class Mask<F, T> extends DerivativeWatchable<F, T> {
 
 	private final Function<? super F, ? extends T> conv;
@@ -11,7 +27,7 @@ public class Mask<F, T> extends DerivativeWatchable<F, T> {
 		return source;
 	}
 
-	private Mask(Function<? super F, ? extends T> converter, Watchable<? extends F> source, boolean weak) {
+	public Mask(Function<? super F, ? extends T> converter, Watchable<? extends F> source, boolean weak) {
 		super(weak);
 		conv = converter;
 		this.source = source;
