f7678e64d92aff5672301ac3f09f956595bd9341
Palanath <pala@dusttoash.org>
2023-03-04 16:32:47 -0600

    Update HomePresentation spec

--- src/pala/apps/arlith/frontend/clientgui/uispec/home/HomePresentation.java
+++ src/pala/apps/arlith/frontend/clientgui/uispec/home/HomePresentation.java
@@ -2,6 +2,16 @@ package pala.apps.arlith.frontend.clientgui.uispec.home;
 
 import pala.apps.arlith.frontend.clientgui.Presentation;
 
+/**
+ * The Home scene is the "main" scene for the client frontend. It shows up
+ * immediately after log in and acts as a "hub" through which other UIs can be
+ * accessed, either directly or indirectly. The Home scene is currently simple
+ * in design. It should show users threads they have access to and allow users
+ * to read and send messages in those threads.
+ * 
+ * @author Palanath
+ *
+ */
 public interface HomePresentation extends Presentation<HomeLogic> {
 
 }
