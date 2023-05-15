c0e0356b99f56cc4ee25941f298cc804cc9f0f71
Palanath <pala@dusttoash.org>
2023-05-15 01:22:19 -0500

    Create LoginWindow.java

---
+++ src/pala/apps/arlith/frontend/clientgui2/login/LoginWindow.java
@@ -0,0 +1,21 @@
+package pala.apps.arlith.frontend.clientgui2.login;
+
+import javafx.stage.Stage;
+import pala.libs.generic.guis.ApplicationProperties;
+import pala.libs.generic.guis.Window;
+
+public class LoginWindow extends Window {
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	protected void show(Stage stage, ApplicationProperties properties) throws WindowLoadFailureException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
