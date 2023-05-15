851c7a186e1bb21b0feccc1f65fdaae38a9bab43
Palanath <pala@dusttoash.org>
2023-05-15 14:07:10 -0500

    Add some demo background to LoginWindow

--- src/pala/apps/arlith/frontend/clientgui2/login/LoginWindow.java
+++ src/pala/apps/arlith/frontend/clientgui2/login/LoginWindow.java
@@ -1,13 +1,18 @@
 package pala.apps.arlith.frontend.clientgui2.login;
 
+import java.io.IOException;
+
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.geometry.Insets;
+import javafx.scene.Scene;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.paint.Color;
 import javafx.scene.paint.CycleMethod;
+import javafx.scene.paint.LinearGradient;
 import javafx.scene.paint.RadialGradient;
 import javafx.scene.paint.Stop;
 import javafx.stage.Stage;
@@ -25,15 +30,35 @@ public class LoginWindow extends Window {
 	}
 
 	private @FXML void initialize() {
-		BackgroundFill layer1 = new BackgroundFill(new RadialGradient(0, 0, 0, 0, 0.5, true, CycleMethod.NO_CYCLE,
-				new Stop(0, Color.web("#3235ff")), new Stop(1, Color.TRANSPARENT)), null, null);
-		BackgroundFill layer2 = new BackgroundFill(Color.ORANGE, new CornerRadii(5), new Insets(5));
-		root.setBackground(new Background(layer1, layer2));
+		BackgroundFill layer1 = new BackgroundFill(new RadialGradient(0, 0, 0, 0, 1.5, true, CycleMethod.NO_CYCLE,
+				new Stop(.6, Color.web("#3235ff")), new Stop(.7, Color.web("#ff8b32"))), null, null);
+		BackgroundFill layer2 = new BackgroundFill(new RadialGradient(0, 0, 0, 1, 1, true, CycleMethod.NO_CYCLE,
+				new Stop(.8, Color.web("#c232ff")), new Stop(1, Color.TRANSPARENT)), null, null);
+
+		// Gray
+		BackgroundFill layer3 = new BackgroundFill(Color.gray(.12), new CornerRadii(5), new Insets(5));
+
+		Color stripeColor = Color.gray(.2);
+		double stripeWidth = .04;
+		BackgroundFill layer4 = new BackgroundFill(new LinearGradient(0, 0, 20, 20, false, CycleMethod.REPEAT,
+				new Stop(0, stripeColor), new Stop(stripeWidth, stripeColor), new Stop(stripeWidth, Color.TRANSPARENT),
+				new Stop(1, Color.TRANSPARENT)), new CornerRadii(5), new Insets(5));
+		BackgroundFill layer5 = new BackgroundFill(new LinearGradient(0, 20, 20, 0, false, CycleMethod.REPEAT,
+				new Stop(0, stripeColor), new Stop(stripeWidth, stripeColor), new Stop(stripeWidth, Color.TRANSPARENT),
+				new Stop(1, Color.TRANSPARENT)), new CornerRadii(5), new Insets(5));
+		root.setBackground(new Background(layer1, layer2, layer3, layer4, layer5));
 	}
 
 	@Override
 	protected void show(Stage stage, ApplicationProperties properties) throws WindowLoadFailureException {
 		stage.show();
+		FXMLLoader loader = new FXMLLoader();
+		loader.setController(this);
+		try {
+			stage.setScene(new Scene(loader.load(getClass().getResourceAsStream("LoginGUI.fxml"))));
+		} catch (IOException e) {
+			throw new WindowLoadFailureException("IOException while loading embedded UI for login window.", e);
+		}
 	}
 
 }
