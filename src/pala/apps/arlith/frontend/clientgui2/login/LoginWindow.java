620c1b475245db7fb796d3afa1d08b391cd0e2c1
Palanath <pala@dusttoash.org>
2023-05-18 20:21:07 -0500

    Update Login window

--- src/pala/apps/arlith/frontend/clientgui2/login/LoginWindow.java
+++ src/pala/apps/arlith/frontend/clientgui2/login/LoginWindow.java
@@ -37,28 +37,25 @@ public class LoginWindow extends Window {
 //				new Stop(.8, Color.web("#c232ff")), new Stop(1, Color.TRANSPARENT)), null, null);
 
 		// Solid Fill
-		CornerRadii innerSectionRadii = null;// new CornerRadii(5);
-		Insets innerSectionInsets = null;// new Insets(5);
-
-		BackgroundFill layer3 = new BackgroundFill(Color.gray(.12), innerSectionRadii, innerSectionInsets);
+		BackgroundFill layer3 = new BackgroundFill(Color.gray(.12), null, null);
 
 		// Stripes
 		Color stripeColor = Color.gray(.2);
 		double stripeWidth = .04;
 		BackgroundFill layer4 = new BackgroundFill(new LinearGradient(0, 0, 20, 20, false, CycleMethod.REPEAT,
 				new Stop(0, stripeColor), new Stop(stripeWidth, stripeColor), new Stop(stripeWidth, Color.TRANSPARENT),
-				new Stop(1, Color.TRANSPARENT)), innerSectionRadii, innerSectionInsets);
+				new Stop(1, Color.TRANSPARENT)), null, null);
 		BackgroundFill layer5 = new BackgroundFill(new LinearGradient(0, 20, 20, 0, false, CycleMethod.REPEAT,
 				new Stop(0, stripeColor), new Stop(stripeWidth, stripeColor), new Stop(stripeWidth, Color.TRANSPARENT),
-				new Stop(1, Color.TRANSPARENT)), innerSectionRadii, innerSectionInsets);
+				new Stop(1, Color.TRANSPARENT)), null, null);
 
 		// Lights
 		BackgroundFill layer6 = new BackgroundFill(new RadialGradient(0, 0, -.25, 0, 1.05, true, CycleMethod.NO_CYCLE,
-				new Stop(0, Color.web("#3235ff3f")), new Stop(1, Color.TRANSPARENT)), innerSectionRadii,
-				innerSectionInsets);
+				new Stop(0, Color.web("#3235ff3f")), new Stop(1, Color.TRANSPARENT)), null,
+				null);
 		BackgroundFill layer7 = new BackgroundFill(new RadialGradient(0, 0, 1.2, .8, .8, true, CycleMethod.NO_CYCLE,
-				new Stop(0, Color.web("#ff8b323f")), new Stop(1, Color.TRANSPARENT)), innerSectionRadii,
-				innerSectionInsets);
+				new Stop(0, Color.web("#ff8b323f")), new Stop(1, Color.TRANSPARENT)), null,
+				null);
 
 		root.setBackground(new Background(layer3, layer4, layer5, layer6, layer7));
 	}
